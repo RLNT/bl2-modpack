31c1a457f4023b06fc7a2d6dbab61ed9ea94f1ea
Relentless <relentless@rlnt.dev>
2021-03-14 21:01:04 +0100

    updated mission selector to support coop

--- files/Win32/Mods/MissionSelector/__init__.py
+++ files/Win32/Mods/MissionSelector/__init__.py
@@ -1,33 +1,51 @@
 import unrealsdk
-import itertools
 import enum
-from Mods.ModMenu import SDKMod, Mods, RegisterMod, ModTypes, EnabledSaveType, KeybindManager, Keybind
+import webbrowser
+from typing import Dict, List, Optional
+from Mods.ModMenu import (
+    SDKMod,
+    Mods,
+    RegisterMod,
+    ModTypes,
+    EnabledSaveType,
+    KeybindManager,
+    Keybind,
+    ServerMethod,
+    ClientMethod,
+)
 
 NEXT_MISSION_DESC: str = "Select next Mission"
 NEXT_MISSION_KEY: str = "RightBracket"
 PREV_MISSION_DESC: str = "Select previous Mission"
-PREV_MISSION_KEY: str = "LEFTBracket"
+PREV_MISSION_KEY: str = "LeftBracket"
+
 
 class EMissionStatus(enum.IntEnum):
     NotStarted = 0
-    Active = enum.auto()
-    RequiredObjectivesComplete = enum.auto()
-    ReadyToTurnIn = enum.auto()
-    Complete = enum.auto()
-    Failed = enum.auto()
-    MAX = enum.auto()
+    Active = 1
+    RequiredObjectivesComplete = 2
+    ReadyToTurnIn = 3
+    Complete = 4
+    Failed = 5
+    MAX = 6
+
+    def isActive(self) -> bool:
+        return self in [
+            EMissionStatus.ReadyToTurnIn,
+            EMissionStatus.Active,
+        ]
 
 
 class MissionSelector(SDKMod):
     Name: str = "Mission Selector"
     Author: str = "Chronophylos"
-    Description: str = (
-        "Switch through missions with hotkeys, like in BL3\n"
-    )
-    Version: str = "1.0.0"
+    Description: str = "Switch through missions with hotkeys, like in BL3\n"
+    Version: str = "1.2.0"
 
     Types: ModTypes = ModTypes.Utility
     SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
+
+    SettingsInputs: Dict[str, str] = {"Enter": "Enable", "G": "Github"}
 
     def __init__(self) -> None:
         super().__init__()
@@ -42,12 +60,18 @@ class MissionSelector(SDKMod):
     def _log(self, message: str) -> None:
         unrealsdk.Log(f"[{self.Name}] {message}")
 
+    @ClientMethod
+    def _logClient(self, message: str, PC: Optional[unrealsdk.UObject] = None) -> None:
+        self._log(message)
+
     def Enable(self) -> None:
         super().Enable()
 
         self._log(f"Version: {self.Version}")
 
-    def GameInputPressed(self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent) -> None:
+    def GameInputPressed(
+        self, bind: KeybindManager.Keybind, event: KeybindManager.InputEvent
+    ) -> None:
         if event != KeybindManager.InputEvent.Pressed:
             return
 
@@ -56,72 +80,104 @@ class MissionSelector(SDKMod):
         elif bind.Name == PREV_MISSION_DESC:
             self.PrevMission()
 
+    def SettingsInputPressed(self, action: str) -> None:
+        super().SettingsInputPressed(action)
+
+        if action == "Github":
+            webbrowser.open("https://github.com/Chronophylos/bl2_missionselector")
+
     def NextMission(self) -> None:
-        activeMission = self._getSelectedMission()
-        missions = self._getActiveMissions()
+        missions = self.GetActiveMissions()
+        active_mission_index = self._getActiveMissionIndex(missions)
 
-        activeMissionIndex = None
-        for i, m in enumerate(missions):
-            if m.MissionDef.MissionNumber == activeMission.MissionNumber:
-                activeMissionIndex = i
-                break
-        
-        nextMission = None
-        if activeMissionIndex < len(missions) - 1:
-            nextMission = missions[activeMissionIndex + 1]
+        next_mission = None
+        if active_mission_index < len(missions) - 1:
+            next_mission = missions[active_mission_index + 1]
         else:
-            nextMission = missions[0]
+            next_mission = missions[0]
 
-
-        self._setSelectedMission(nextMission.missionDef)
+        self.SetSelectedMission(next_mission.missionDef)
 
     def PrevMission(self) -> None:
-        activeMission = self._getSelectedMission()
-        missions = self._getActiveMissions()
+        missions = self.GetActiveMissions()
+        active_mission_index = self._getActiveMissionIndex(missions)
 
-        activeMissionIndex = None
-        for i, m in enumerate(missions):
-            if m.MissionDef.MissionNumber == activeMission.MissionNumber:
-                activeMissionIndex = i
-                break
-        
-        nextMission = missions[activeMissionIndex - 1]
+        next_mission = missions[active_mission_index - 1]
 
-        self._setSelectedMission(nextMission.missionDef)
+        self.SetSelectedMission(next_mission.missionDef)
+
+    def _isClient(self) -> bool:
+        return int(unrealsdk.GetEngine().GetCurrentWorldInfo().NetMode) == 3
 
     def _getMissionTracker(self) -> unrealsdk.UObject:
-        """Returns the `WillowGame.MissionTracker`."""
+        """Return the `WillowGame.MissionTracker`."""
         return unrealsdk.GetEngine().GetCurrentWorldInfo().GRI.MissionTracker
 
-    def _getActiveMissions(self) -> [unrealsdk.UObject]:
+    def _getActiveMissionIndex(self, missions: List[unrealsdk.UObject]) -> int:
+        active_mission = self.GetSelectedMission()
+        for i, m in enumerate(missions):
+            if m.MissionDef.MissionNumber == active_mission.MissionNumber:
+                return i
+        return -1
+
+    def GetActiveMissions(self) -> List[unrealsdk.UObject]:
         """
         Get all missions that are either active or ready to turn in.
         Returns list of `IMission.MissionData`
         """
         missions = []
         for mission in self._getMissionTracker().MissionList:
-            if mission.Status in [EMissionStatus.ReadyToTurnIn, EMissionStatus.Active]:
+            if EMissionStatus(mission.Status).isActive():
                 missions.append(mission)
-        return missions
+        return sorted(missions, key=lambda mission: int(mission.ExpLevel))
 
-    def _getSelectedMission(self) -> unrealsdk.UObject:
-        """Returns the selected mission as `WillowGame.MissionDefinition`."""
+    def GetSelectedMission(self) -> unrealsdk.UObject:
+        """Return the selected mission as `WillowGame.MissionDefinition`."""
         return self._getMissionTracker().GetActiveMission()
 
-    def _setSelectedMission(self, missionDef: unrealsdk.UObject) -> None:
-        """Sets the selected mission. mission must be a `WillowGame.MissionDefinition`."""
-        self._getMissionTracker().SetActiveMission(missionDef)
-        self._log(f"Set active mission to {missionDef.MissionName}")
+    def GetMissionByNumber(self, number: int) -> Optional[unrealsdk.UObject]:
+        for mission in self._getMissionTracker().MissionList:
+            if mission.MissionDef.MissionNumber == number:
+                return mission.MissionDef
+        return None
+
+    def SetSelectedMission(self, mission: unrealsdk.UObject) -> None:
+        """Sets the selected mission.
+
+        mission must be a `WillowGame.MissionDefinition`.
+        """
+
+        if self._isClient():
+            self._serverSetSelectedMission(mission.MissionNumber)
+        else:
+            self._setSelectedMission(mission.MissionNumber)
+
+    @ServerMethod
+    def _serverSetSelectedMission(
+        self, number: int, PC: Optional[unrealsdk.UObject] = None
+    ) -> None:
+        self._setSelectedMission(number, PC)
+
+    def _setSelectedMission(
+        self, number: int, PC: Optional[unrealsdk.UObject] = None
+    ) -> None:
+        mission = self.GetMissionByNumber(number)
+        if not mission:
+            self._log("Could not find mission with number {number}")
+            return
+        self._logClient(f"Set active mission to {mission.MissionName}", PC)
+        self._getMissionTracker().SetActiveMission(mission, True, PC)
+
 
 instance = MissionSelector()
 if __name__ == "__main__":
-    instance._log(f"Manually loaded")
+    instance._log("Manually loaded")
     for mod in Mods:
         if mod.Name == instance.Name:
             if mod.IsEnabled:
                 mod.Disable()
             Mods.remove(mod)
-            instance._log(f"Removed last instance")
+            instance._log("Removed last instance")
 
             # Fixes inspect.getfile()
             instance.__class__.__module__ = mod.__class__.__module__
